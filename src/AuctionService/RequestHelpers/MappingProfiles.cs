bf0b97d50829186c9dbf315637e337704c789ea0
Laurie Roy <laurieroy.dev@gmail.com>
2023-07-29 15:49:10 -0500

    Map DTOs

---
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+
+namespace AuctionService.RequestHelpers;
+
+public class MappingProfiles: Profile
+{
+public MappingProfiles()
+{
+    CreateMap<Auction, AuctionDTO>().IncludeMembers(x => x.Item);
+    CreateMap<AuctionDTO, Auction>();
+    CreateMap<CreateAuctionDTO, Auction>().ForMember(d => d.Item, o=>o.MapFrom(s => s));
+    CreateMap<AuctionDTO, Item>();
+}
+}
