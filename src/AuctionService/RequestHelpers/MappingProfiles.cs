166d80bea6ccb79919325a0e2178eb243ed9b51c
Laurie Roy <laurieroy.dev@gmail.com>
2023-07-29 18:06:59 -0500

    Update mapping profile

--- src/AuctionService/RequestHelpers/MappingProfiles.cs
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -6,11 +6,11 @@ namespace AuctionService.RequestHelpers;
 
 public class MappingProfiles: Profile
 {
-public MappingProfiles()
-{
-    CreateMap<Auction, AuctionDTO>().IncludeMembers(x => x.Item);
-    CreateMap<AuctionDTO, Auction>();
-    CreateMap<CreateAuctionDTO, Auction>().ForMember(d => d.Item, o=>o.MapFrom(s => s));
-    CreateMap<AuctionDTO, Item>();
-}
+    public MappingProfiles()
+    {
+        CreateMap<Auction, AuctionDTO>().IncludeMembers(x => x.Item);
+        CreateMap<Item, AuctionDTO>();
+        CreateMap<CreateAuctionDTO, Auction>().ForMember(d => d.Item, o => o.MapFrom(s => s));
+        CreateMap<CreateAuctionDTO, Item>();
+    }
 }
