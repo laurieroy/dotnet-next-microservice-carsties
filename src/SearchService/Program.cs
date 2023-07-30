5c4b1f69e4b6286942fb92316c2bdc321ae5efe9
Laurie Roy <laurieroy.dev@gmail.com>
2023-07-29 19:38:58 -0500

    Add mongo connection + Item model

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,10 +13,15 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-
-
 app.UseAuthorization();
 
 app.MapControllers();
+
+await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
+await DB.Index<Item>()
+    .Key(x => x.Make, KeyType.Text)
+    .Key(x => x.Model, KeyType.Text)
+    .Key(x => x.Color, KeyType.Text)
+    .CreateAsync();
 
 app.Run();
